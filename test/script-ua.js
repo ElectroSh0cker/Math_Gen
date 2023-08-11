4747d67de6c4c36216163ef3195ae5c2ae4d3bd3
AN656 <108411199+AN656@users.noreply.github.com>
2023-08-11 21:33:41 +0300

    Add files via upload

---
+++ test/script-ua.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, "Потрібно ввести псевдонім користувача");
+    } else {
+        setSuccess(username);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Потрібен пароль');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Пароль має бути не менше 8 символів')
+    } else {
+        setSuccess(password);
+    }
+
+};
